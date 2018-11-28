ff4f46b967db5d3ab4df0cef4ab7efde20c75555
Niall Maher <frostie.maher92@gmail.com>
2018-11-28 10:24:49 +0000

    Update app_config.groovy

--- vars/app_config.groovy
+++ vars/app_config.groovy
@@ -7,7 +7,7 @@ def bob(opt) {
                 '--env RELEASE=true ' +
                 "-v \"`pwd`:`pwd`\" " +
                 "-v /var/run/docker.sock:/var/run/docker.sock " +
-   "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:1.4.0-8" ${opt}
+   "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:1.4.0-8 ${opt}" 
 }
 
 def test(name) {
