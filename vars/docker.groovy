5d37106f31b239d1d566ea002df5503c0e8417a6
Niall Maher <frostie.maher92@gmail.com>
2018-11-28 15:15:14 +0000

    Update and rename app_config.groovy to docker.groovy

---
+++ vars/docker.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/groovy
+package com.cleverbuilder
+
+def bob(opt) {
+ sh "docker run --rm " +
+         '--env APP_PATH="`pwd`" ' +
+         '--env RELEASE=true ' +
+         "-v \"`pwd`:`pwd`\" " +
+         "-v /var/run/docker.sock:/var/run/docker.sock " +
+         "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:1.4.0-8 ${opt}" 
+}
+
+def gradle(){
+sh "docker run -u root --rm " +
+        "-v \"`pwd`:/home/gradle/project\" " +
+        "-w \"/home/gradle/project/eso_workspace/projects/oss-eso-mt\" " +
+        "armdocker.rnd.ericsson.se/proj-orchestration-so/so-gradle-image"
+}
+
+def unified_installer(eaiVersion){
+sh "docker run -u root --rm " +
+        "-v \"`pwd`/outputs:/root/USER_INSTALL_DIR\" " +
+        "-v \"`pwd`/eso_workspace/build/distributions:/root/CUSTOMER_GPARS\" " +
+        "armdocker.rnd.ericsson.se/proj-orchestration-so/eai_unified_installer:${eaiVersion}"
+}
+
+def GIT_CMD(){
+sh "docker run --rm -v ${env.WORKSPACE}:/git alpine/git"
+}
+
+def HELM_CMD(){
+sh "docker run --rm " +
+        "-v ${env.WORKSPACE}/.kube/config:/root/.kube/config " +
+        "-v ${env.WORKSPACE}/helm-home:/root/.helm " +
+        "-v ${env.WORKSPACE}:${env.WORKSPACE} linkyard/docker-helm:2.10.0"
+}
+
+
+def KUBE_CMD(){
+sh "docker run --rm " +
+        "-v ${env.WORKSPACE}/.kube/config:/config/.kube/config " +
+        "-v ${env.WORKSPACE}:${env.WORKSPACE} " +
+        "-e KUBECONFIG=/config/.kube/config lachlanevenson/k8s-kubectl:v1.11.2"
+}
