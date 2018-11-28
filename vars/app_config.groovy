75d247697b499169789c40899f9eb4e8a59d6a47
Niall Maher <frostie.maher92@gmail.com>
2018-11-28 10:23:50 +0000

    Update app_config.groovy

--- vars/app_config.groovy
+++ vars/app_config.groovy
@@ -4,7 +4,7 @@ package com.cleverbuilder
 def bob(opt) {
  echo "docker run --rm " +
                 '--env APP_PATH="`pwd`" ' +
-                '--env RELEASE=${RELEASE} ' +
+                '--env RELEASE=true ' +
                 "-v \"`pwd`:`pwd`\" " +
                 "-v /var/run/docker.sock:/var/run/docker.sock " +
    "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:1.4.0-8" ${opt}
