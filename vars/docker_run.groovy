f49e1ce050783811dd42dea0ae5ca27f19b316f3
Niall Maher <frostie.maher92@gmail.com>
2018-11-29 09:00:31 +0000

    Update docker_run.groovy

--- vars/docker_run.groovy
+++ vars/docker_run.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/groovy
 package com.cleverbuilder
 
-def bob(operation) {
- sh "docker run --rm " +
+def bob() {
+  "docker run --rm " +
          '--env APP_PATH="`pwd`" ' +
          '--env RELEASE=true ' +
          "-v \"`pwd`:`pwd`\" " +
          "-v /var/run/docker.sock:/var/run/docker.sock " +
-         "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:1.4.0-8 ${operation}" 
+         "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:1.4.0-8" 
 }
 
 def gradle(operation){
