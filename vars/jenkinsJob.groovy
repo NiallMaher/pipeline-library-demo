abe80840580353fb4d5d0e24b8e7d29932a141e4
Niall Maher <frostie.maher92@gmail.com>
2018-12-18 10:36:01 +0000

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,9 +1,18 @@
 def call() {
+    // Execute build pipeline job
+    build_pipeline()
+}
 
-    parameters {
+def build_pipeline() {
+    pipeline {
+        agent {
+            node {
+                label params.SLAVE
+            }
+        }
+        parameters {
             string(name: 'SETTINGS_CONFIG_FILE_NAME', defaultValue: 'maven.settings.eso')
             string(name: 'BOB_VERSION', defaultValue: '1.4.0-8')
-            string(name: 'SLAVE', defaultValue: 'so_slave')
         }
 
         environment {
@@ -15,75 +24,67 @@ def call() {
                     "armdocker.rnd.ericsson.se/proj-orchestration-so/bob:${BOB_VERSION}"
         }
 
-        // Execute build pipeline job
-        build_pipeline()
-
-    }
-
-def build_pipeline() {
-    pipeline {
-        
-    stages {
-        stage('Inject Settings.xml File') {
-            steps {
-                configFileProvider([configFile(fileId: "${env.SETTINGS_CONFIG_FILE_NAME}", targetLocation: "${env.WORKSPACE}")]) {
+        stages {
+            stage('Inject Settings.xml File') {
+                steps {
+                    configFileProvider([configFile(fileId: "${env.SETTINGS_CONFIG_FILE_NAME}", targetLocation: "${env.WORKSPACE}")]) {
+                    }
+                }
+            }
+            stage('Clean') {
+                steps {
+                    sh "${bob} clean"
+                    sh 'git clean -xdff --exclude=.m2 --exclude=.sonar --exclude=settings.xml'
+                }
+            }
+            stage('Lint') {
+                steps {
+                    sh "${bob} lint"
+                }
+            }
+            stage('Build package and execute tests') {
+                steps {
+                    sh "${bob} build"
+                }
+            }
+            stage('Build image and chart') {
+                steps {
+                    sh "${bob} image"
+                }
+            }
+            stage('SonarQube full analysis') {
+                when {
+                    expression { params.RELEASE == "true" }
+                }
+                steps {
+                    sh "${bob} sonar"
+                }
+            }
+            stage('Push image and chart') {
+                when {
+                    expression { params.RELEASE == "true" }
+                }
+                steps {
+                    sh "${bob} push"
+                }
+            }
+            stage('Generate input for ESO staging') {
+                when {
+                    expression { params.RELEASE == "true" }
+                }
+                steps {
+                    sh "${bob} generate-output-parameters"
+                    archiveArtifacts 'artifact.properties'
                 }
             }
         }
-        stage('Clean') {
-            steps {
-                sh "${bob} clean"
-                sh 'git clean -xdff --exclude=.m2 --exclude=.sonar --exclude=settings.xml'
-            }
-        }
-        stage('Lint') {
-            steps {
-                sh "${bob} lint"
-            }
-        }
-        stage('Build package and execute tests') {
-            steps {
-                sh "${bob} build"
-            }
-        }
-        stage('Build image and chart') {
-            steps {
-                sh "${bob} image"
-            }
-        }
-        stage('SonarQube full analysis') {
-            when {
-                expression { params.RELEASE == "true" }
-            }
-            steps {
-                sh "${bob} sonar"
-            }
-        }
-        stage('Push image and chart') {
-            when {
-                expression { params.RELEASE == "true" }
-            }
-            steps {
-                sh "${bob} push"
-            }
-        }
-        stage('Generate input for ESO staging') {
-            when {
-                expression { params.RELEASE == "true" }
-            }
-            steps {
-                sh "${bob} generate-output-parameters"
-                archiveArtifacts 'artifact.properties'
-            }
-        }
-    }
 
-    post {
-        always {
-            archive "**/target/surefire-reports/*"
-            junit '**/target/surefire-reports/*.xml'
-            step([$class: 'JacocoPublisher'])
+        post {
+            always {
+                archive "**/target/surefire-reports/*"
+                junit '**/target/surefire-reports/*.xml'
+                step([$class: 'JacocoPublisher'])
+            }
         }
     }
-}
 }
