ab93989c774a3bf055d73b84b0e1b9d6863c4fdc
Niall Maher <frostie.maher92@gmail.com>
2018-12-18 13:08:56 +0000

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -5,11 +5,6 @@ def call() {
 
 def build_pipeline() {
 pipeline {
-    agent {
-        node {
-            label params.SLAVE
-        }
-    }
         parameters {
             string(name: 'SETTINGS_CONFIG_FILE_NAME', defaultValue: 'maven.settings.eso')
             string(name: 'BOB_VERSION', defaultValue: '1.4.0-8')
